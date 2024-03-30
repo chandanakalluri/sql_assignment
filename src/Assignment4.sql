bc080eadd23f87e1f3cb6689f8c496da61b39840
chand796 <162735284+chandanakalluri@users.noreply.github.com>
2024-03-31 02:20:31 +0530

    latest and first signup date and difference between both the dates and also write the query to replace null value

--- src/Assignment4.sql
+++ src/Assignment4.sql
@@ -1,3 +1,14 @@
+--Write a query to find gmail accounts with latest and first signup date and difference between both the dates and also write the query to replace null value with ‘1970-01-01’
+--Column names: (id, email_id, signup_date)
+--Data:
+--(1, 'Rajesh@Gmail.com', '2022-02-01'),
+--(2, 'Rakesh_gmail@rediffmail.com', '2023-01-22'),
+--(3, 'Hitest@Gmail.com', '2020-09-08'),
+--(4, 'Salil@Gmmail.com', '2019-07-05'),
+--(5, 'Himanshu@Yahoo.com', '2023-05-09'),
+--(6, 'Hitesh@Twitter.com', '2015-01-01'),
+--(7, 'Rakesh@facebook.com', null);
+
 use ecommerce
 SELECT*FROM email_signups;
 SELECT 
